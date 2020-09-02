fe52ada96b25e775b1fa2393189f550b022b55a5
vhamilton08 <vhamilton08@gmail.com>
2020-09-02 16:18:40 -0500

    a

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,13 +6,16 @@ const Product = (props) => {
     return (
 <div className='product'>
 
-        <div className='product'>
+        <div className='productpic'>
             <div key={props.index}></div>
             <img className='productimage' alt='menu item' src={props.element.image}/>
         </div>
+        <div className='infocontainer'>
 
             <h2>{props.element.name}</h2>
             <h3>${props.element.price}</h3>
+            <button className='cartbutton'>Add to Cart</button>
+        </div>
 </div>
 
     )
